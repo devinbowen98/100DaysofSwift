8ad388b80633f4df651b0df5a9609bfeea9706d9
Devin Bowen <devin.bowen7844@stu.mtec.edu>
2020-09-15 10:20:20 -0600

    Create Day 16 project

---
+++ Day16/Day16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day16
+//
+//  Created by Devin Bowen on 9/15/20.
+//  Copyright © 2020 Devin Bowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
